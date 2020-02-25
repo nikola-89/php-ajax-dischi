6c851a761d0b803ff91e9fd5c5ab33e816b5242f
Nicola <nicola.ba1989@gmail.com>
2020-02-25 16:44:46 +0100

    init

---
+++ database.php
@@ -0,0 +1,64 @@
+<?php
+    $database = [
+        [
+            "title" => "A Night At The Opera (Deluxe Edition 2011 Remaster)",
+            "author" => "Queen",
+            "year" => 1975,
+            "poster" => "https://i.scdn.co/image/356e29a0f112a4835f7b22f5e586a1907eae2031"
+        ],
+        [
+            "title" => "Bunny Wailer Sings The Wailers",
+            "author" => "Bunny Wailer",
+            "year" => 1980,
+            "poster" => "https://i.scdn.co/image/ab67616d0000b273d159291c862cafdcdc69cfd8"
+        ],
+        [
+            "title" => "Legend - The Best Of Bob Marley And The Wailers",
+            "author" => "Bob Marley & The Wailers",
+            "year" => 2002,
+            "poster" => "https://i.scdn.co/image/ab67616d0000b273413a6c2c7b296d98171e5e21"
+        ],
+        [
+            "title" => "Let It Bleed (50th Anniversary Edition / Remastered 2019)",
+            "author" => "The Rolling Stones",
+            "year" => 1969,
+            "poster" => "https://i.scdn.co/image/4c58dd71561434e9aded2f3a7ddb5346f750eb1e"
+        ],
+        [
+            "title" => "Master Of Puppets (Deluxe Box Set / Remastered)",
+            "author" => "Metallica",
+            "year" => 1986,
+            "poster" => "https://i.scdn.co/image/ab67616d0000b27316d6f039fafd389911261ada"
+        ],
+        [
+            "title" => "Sadnecessary",
+            "author" => "Milky Chance",
+            "year" => 2013,
+            "poster" => "https://i.scdn.co/image/ab67616d0000b27323444c767d67417474f103e3"
+        ],
+        [
+            "title" => "Supersantos",
+            "author" => "Mannarino",
+            "year" => 2011,
+            "poster" => "https://i.scdn.co/image/ab67616d0000b2733954001ba6f75f173f26dce3"
+        ],
+        [
+            "title" => "Terra",
+            "author" => "Le Luci Della Centrale Elettrica",
+            "year" => 2017,
+            "poster" => "https://i.scdn.co/image/ab67616d0000b27397db267cdc24d5786f601349"
+        ],
+        [
+            "title" => "The Lumineers (Deluxe Edition)",
+            "author" => "The Lumineers",
+            "year" => 2012,
+            "poster" => "https://i.scdn.co/image/46b171f5f29b5bdb64a820636b2914927cfeb274"
+        ],
+        [
+            "title" => "The Razors Edge",
+            "author" => "AC/DC",
+            "year" => 1990,
+            "poster" => "https://i.scdn.co/image/ab67616d0000b273449b39efb3f857f936fcc305"
+        ]
+    ]
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    include __DIR__ . '/database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" sizes="32x32" type="image/png" href="https://open.scdn.co/cdn/images/favicon32.a19b4f5b.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="dist/app.css">
+        <script src="dist/app.js" charset="utf-8"></script>
+        <title>PHP - Spotify</title>
+    </head>
+    <body>
+    </body>
+</html>
