3e5b51865a0039660135e36122c592fd8bf7b933
Nicola <nicola.ba1989@gmail.com>
2020-02-25 22:37:05 +0100

    [PHP] | db

--- database.php
+++ database.php
@@ -1,94 +1,106 @@
 <?php
     $database = [
         [
-            "title" => "A Night At The Opera (Deluxe Edition 2011 Remaster)",
-            "author" => "Queen",
-            "year" => 1975,
-            "poster" => "https://i.scdn.co/image/356e29a0f112a4835f7b22f5e586a1907eae2031"
+            'title' => 'A Night At The Opera (Deluxe Edition 2011 Remaster)',
+            'author' => 'Queen',
+            'year' => 1975,
+            'poster' => 'https://i.scdn.co/image/356e29a0f112a4835f7b22f5e586a1907eae2031'
         ],
         [
-            "title" => "Bunny Wailer Sings The Wailers",
-            "author" => "Bunny Wailer",
-            "year" => 1980,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b273d159291c862cafdcdc69cfd8"
+            'title' => 'Bunny Wailer Sings The Wailers',
+            'author' => 'Bunny Wailer',
+            'year' => 1980,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b273d159291c862cafdcdc69cfd8'
         ],
         [
-            "title" => "Legend - The Best Of Bob Marley And The Wailers",
-            "author" => "Bob Marley & The Wailers",
-            "year" => 2002,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b273413a6c2c7b296d98171e5e21"
+            'title' => 'Legend - The Best Of Bob Marley And The Wailers',
+            'author' => 'Bob Marley & The Wailers',
+            'year' => 2002,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b273413a6c2c7b296d98171e5e21'
         ],
         [
-            "title" => "Let It Bleed (50th Anniversary Edition / Remastered 2019)",
-            "author" => "The Rolling Stones",
-            "year" => 1969,
-            "poster" => "https://i.scdn.co/image/4c58dd71561434e9aded2f3a7ddb5346f750eb1e"
+            'title' => 'Let It Bleed (50th Anniversary Edition / Remastered 2019)',
+            'author' => 'The Rolling Stones',
+            'year' => 1969,
+            'poster' => 'https://i.scdn.co/image/4c58dd71561434e9aded2f3a7ddb5346f750eb1e'
         ],
         [
-            "title" => "Master Of Puppets (Deluxe Box Set / Remastered)",
-            "author" => "Metallica",
-            "year" => 1986,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b27316d6f039fafd389911261ada"
+            'title' => 'Master Of Puppets (Deluxe Box Set / Remastered)',
+            'author' => 'Metallica',
+            'year' => 1986,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b27316d6f039fafd389911261ada'
         ],
         [
-            "title" => "Sadnecessary",
-            "author" => "Milky Chance",
-            "year" => 2013,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b27323444c767d67417474f103e3"
+            'title' => 'Sadnecessary',
+            'author' => 'Milky Chance',
+            'year' => 2013,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b27323444c767d67417474f103e3'
         ],
         [
-            "title" => "Supersantos",
-            "author" => "Mannarino",
-            "year" => 2011,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b2733954001ba6f75f173f26dce3"
+            'title' => 'Supersantos',
+            'author' => 'Mannarino',
+            'year' => 2011,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b2733954001ba6f75f173f26dce3'
         ],
         [
-            "title" => "Terra",
-            "author" => "Le Luci Della Centrale Elettrica",
-            "year" => 2017,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b27397db267cdc24d5786f601349"
+            'title' => 'Terra',
+            'author' => 'Le Luci Della Centrale Elettrica',
+            'year' => 2017,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b27397db267cdc24d5786f601349'
         ],
         [
-            "title" => "The Lumineers (Deluxe Edition)",
-            "author" => "The Lumineers",
-            "year" => 2012,
-            "poster" => "https://i.scdn.co/image/46b171f5f29b5bdb64a820636b2914927cfeb274"
+            'title' => 'The Lumineers (Deluxe Edition)',
+            'author' => 'The Lumineers',
+            'year' => 2012,
+            'poster' => 'https://i.scdn.co/image/46b171f5f29b5bdb64a820636b2914927cfeb274'
         ],
         [
-            "title" => "The Razors Edge",
-            "author" => "AC/DC",
-            "year" => 1990,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b273449b39efb3f857f936fcc305"
+            'title' => 'The Razors Edge',
+            'author' => 'AC/DC',
+            'year' => 1990,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b273449b39efb3f857f936fcc305'
         ],
         [
-            "title" => "Elephant",
-            "author" => "The White Stripes",
-            "year" => 2003,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b2733db111787584011d628de58a"
+            'title' => 'Elephant',
+            'author' => 'The White Stripes',
+            'year' => 2003,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b2733db111787584011d628de58a'
         ],
         [
-            "title" => "Mama Africa",
-            "author" => "Peter Tosh",
-            "year" => 1983,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b2739c33fc76509d3b3e89bd5ce2"
+            'title' => 'Mama Africa',
+            'author' => 'Peter Tosh',
+            'year' => 1983,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b2739c33fc76509d3b3e89bd5ce2'
         ],
         [
-            "title" => "Una Mattina",
-            "author" => "Ludovico Einaudi",
-            "year" => 2004,
-            "poster" => "https://i.scdn.co/image/91e8125956d9c8db86f812f052847e93b66f93c7"
+            'title' => 'Una Mattina',
+            'author' => 'Ludovico Einaudi',
+            'year' => 2004,
+            'poster' => 'https://i.scdn.co/image/91e8125956d9c8db86f812f052847e93b66f93c7'
         ],
         [
-            "title" => "Wanted on Voyage",
-            "author" => "George Ezra",
-            "year" => 2014,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b2732b01e29ac167ad5be48f03ef"
+            'title' => 'Wanted on Voyage',
+            'author' => 'George Ezra',
+            'year' => 2014,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b2732b01e29ac167ad5be48f03ef'
         ],
         [
-            "title" => "Stadium Arcadium",
-            "author" => "Red Hot Chili Peppers",
-            "year" => 2006,
-            "poster" => "https://i.scdn.co/image/ab67616d0000b27309fd83d32aee93dceba78517"
+            'title' => 'Stadium Arcadium',
+            'author' => 'Red Hot Chili Peppers',
+            'year' => 2006,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b27309fd83d32aee93dceba78517'
+        ],
+        [
+            'title' => 'L\'Amour Toujours',
+            'author' => 'Gigi D\'Agostino',
+            'year' => 2006,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b2739f2ee3f526327adefd970bc9'
+        ],
+        [
+            'title' => 'They Don\'t Want What We Want (And They Don\'t Care)',
+            'author' => 'Asking Alexandria',
+            'year' => 2020,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b27394eb091c2d81b671b498cb46'
         ],
     ]
 ?>
