e6294e87af3a00e271325f6c2562c7a2ac6b462e
Nicola <nicola.ba1989@gmail.com>
2020-02-25 17:25:39 +0100

    [ALL] | update

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <head>
         <meta charset="UTF-8">
         <link rel="icon" sizes="32x32" type="image/png" href="https://open.scdn.co/cdn/images/favicon32.a19b4f5b.png">
+        <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="dist/app.css">
@@ -14,5 +15,30 @@
         <title>PHP - Spotify</title>
     </head>
     <body>
+        <header>
+            <div class="header">
+                <div class="wrapper">
+                    <div class="navbar">
+                        <div class="logo">
+                            <img src="https://i.imgur.com/1Yn1xQ6.png" alt="logo"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="wrapper">
+                <div class="cds-container">
+                    <?php foreach ($database as $album) { ?>
+                        <div class="cd">
+                            <img src="<?php echo $album['poster']; ?>" alt="<?php echo $album['title']; ?>">
+                            <h3><?php echo $album['title']; ?></h3>
+                            <span class="author"><?php echo $album['author']; ?></span>
+                            <span class="year"><?php echo $album['year']; ?></span>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </main>
     </body>
 </html>
