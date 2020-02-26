a34a1d4fe9ed4de2ca41656dfee7bb605c820ade
Nicola <nicola.ba1989@gmail.com>
2020-02-26 12:51:42 +0100

    [SASS] | update

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <head>
         <meta charset="UTF-8">
         <link rel="icon" sizes="32x32" type="image/png" href="https://open.scdn.co/cdn/images/favicon32.a19b4f5b.png">
-        <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="dist/app.css">
